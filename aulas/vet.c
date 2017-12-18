8d40a6b2b8103dabeb69ee42840fbce984f3ecf1
MatheusSanchez <matheus2.sanchez@usp.br>
2017-12-18 19:44:12 -0200

    Inclusão de comentários no arquivo que gera o vetor

--- aulas/vet.c
+++ aulas/vet.c
@@ -1,6 +1,6 @@
 #include "vet.h"
 
-int* vet_aleatorio(int n){
+int* vet_aleatorio(int n){ // gera um vetor aleatório entre 0 e 1000 (RANGE)
 
 	int *v = malloc(sizeof(int)*n);
 	for(int i = 0; i < n; i++){
@@ -10,14 +10,14 @@ int* vet_aleatorio(int n){
 	return v;
 }
 
-void print_vet(int *vet,int n){
+void print_vet(int *vet,int n){ // printa o vetor
 	for (int i = 0; i < n; ++i){
 		printf("%d-", vet[i]);
 	}
 	printf("\n");
 }
 
-void reset_vet(int *vet,int n){
+void reset_vet(int *vet,int n){ // torna o vetor aleatório novamente;
 	for(int i = 0; i < n; i++){
 		vet[i] = rand() % RANGE;
 	}
